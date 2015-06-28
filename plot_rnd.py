c9967aedbafe5b533e01ae0fa76d1662c9be7598
okloong <okloong88@1utar.edu.my>
2015-06-28 10:48:23 +0800

    Added python.png

--- plot_rnd.py
+++ plot_rnd.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 # Replace '1234' in the bracket with your numeric ID
 np.random.seed(805689)
 
-n = 100
+n = 300
 x = np.random.random(n)
 y = np.random.random(n)
 c = np.random.random(n)  # color of points
-s = 500 * np.random.random(n)  # size of points
+s = 100 * np.random.random(n)  # size of points
 
 fig, ax = plt.subplots()
 im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
